c7927a2b7087aa804fcd3054d0a3df51f0b520a4
benjamin <benjaoyama@gmail.com>
2022-07-17 11:29:21 -0400

    simple push

--- first.py
+++ first.py
@@ -1,7 +1,46 @@
-print("hola diego")
-nombre = input("ingresa tu nombre: ")
 
 
-#typecasting
+def prom(a, b):
+	p =(a+b)/2
+	return  p
 
+def suma(a,b):
+	s=a+b
+	return s
+def resta(a,b):
+	c = a-b
+	return c
+
+def mayorq5(a):
+	if a>5:
+		return "es mayor que 5"
+	else:
+		return "a no es mayor que 5"
+
+
+
+print(mayorq5(10))
+print(mayorq5(15))
+print(mayorq5(1))
+print(mayorq5(8))
+print(mayorq5(9))
+
+
+
+print(prom(6,7))
+print(prom(4,5))
+print(prom(2,7))
+
+print((6+7)/2)
+print((4+5)/2)
+print((2+7)/2)
+print((7+7)/2)
+print((4+4)/2)
+print((4+4)/2)
+print((4+4)/2)
+print((4+4)/2)
+print((4+4)/2)
+print((4+4)/2)
+print((4+4)/2)
+print((4+4)/2)
 
---
+++ second.py
@@ -0,0 +1,11 @@
+a= 1
+def g():
+	a = 2
+	print(a)
+g()
+if(True):
+	a= 2
+	print(a)
+
+
+print(a)
